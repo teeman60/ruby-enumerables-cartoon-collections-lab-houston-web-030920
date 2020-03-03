bd31da989ea62bbc219dc769a98466a8d9c013c2
Taiye Salami <taiye41@gmail.com>
2020-03-03 19:57:53 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |name, i|
+    puts "#{i+1}. #{name}"
+
+  end
+
+end
+# binding.pry
+
+def summon_captain_planet(veggies)# code an argument here
+new_arr = []
+  # Your code here
+  veggies.each do |ele|
+    new_arr << "#{ele.capitalize}!"
+  end
+
+  return new_arr
+
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(calls_long)# code an argument here
   # Your code here
+  calls_long.any? {|word| word.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(food)# code an argument here
   # the array below is here to help
+  cheese = []
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.each do |ele|
+    if ele == "cheddar" || ele == "gouda" || ele == "camembert"
+      cheese << ele
+      return cheese[0]
+
+    end
+  end
+   return nil
 end
