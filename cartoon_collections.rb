3d23f2d4d3227f39fb488c2a3f7f9bcf6e216fe8
Taiye Salami <taiye41@gmail.com>
2020-03-03 19:17:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,15 +9,16 @@ def roll_call_dwarves(dwarves)# code an argument here
 
 end
 # binding.pry
-# def summon_captain_planet# code an argument here
-#   # Your code here
-# end
-#
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
-#
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
+def summon_captain_planet(veggies)# code an argument here
+  # Your code here
+  
+end
+
+def long_planeteer_calls# code an argument here
+  # Your code here
+end
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
